af55c2c34d361b5b1c391f4e8606df05dbef84e1
Landon Clark <landon.d.clark@gmail.com>
2019-01-23 14:23:06 -0700

    Create StructureViewModel.cs

---
+++ ViewModels/StructureViewModel.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+
+namespace PlanCheck
+{
+    public class StructureViewModel : ViewModelBase
+    {
+        public string StructureName { get; set; }
+        public string StructureCode { get; set; }
+        public string StructureNameWithCode { get; set; }
+        public Structure Structure { get; set; }
+        public string VolumeValue { get; set; }
+        public string VolumeUnit {get;set;}
+
+        public StructureViewModel(Structure structure)
+        {
+            if (structure != null)
+            {
+                StructureName = structure.Id;
+                StructureCode = structure.StructureCodeInfos.FirstOrDefault().Code;
+                StructureNameWithCode = StructureName + " : " + StructureCode;
+                Structure = structure;
+                VolumeValue = structure.Volume.ToString("0.0");
+                VolumeUnit = VolumePresentation.AbsoluteCm3.ToString();
+            }
+        }
+    }
+}
