bab62976ad18584435071d6ae06557c2a4580a81
Landon Clark <landon.d.clark@gmail.com>
2019-01-23 14:36:14 -0700

    Delete ErrorViewModel.cs

--- ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PlanCheck
-{
-    public class ErrorViewModel : ViewModelBase
-    {
-        public string Description { get; set; }
-        public string Status { get; set; }
-        public int Severity { get; set; }
-    }
-}
