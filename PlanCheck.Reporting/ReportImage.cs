2d7acb7db44fd7b46cc266bdf1c4679d84aece8f
Landon Clark <landon.d.clark@gmail.com>
2019-01-24 11:56:09 -0700

    Create ReportImage.cs

---
+++ PlanCheck.Reporting/ReportImage.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace PlanCheck.Reporting
+{
+    public class ReportImage
+    {
+        public string Id { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
