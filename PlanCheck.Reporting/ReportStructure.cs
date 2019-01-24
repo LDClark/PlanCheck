b45f3a69accd76af1149e7515a079f24f40b1bfa
Landon Clark <landon.d.clark@gmail.com>
2019-01-24 11:57:26 -0700

    Create ReportStructure.cs

---
+++ PlanCheck.Reporting/ReportStructure.cs
@@ -0,0 +1,9 @@
+namespace PlanCheck.Reporting
+{
+    public class ReportStructure
+    {
+        public string Id { get; set; }
+        public double VolumeInCc { get; set; }
+        public double MeanDoseInGy { get; set; }
+    }
+}
