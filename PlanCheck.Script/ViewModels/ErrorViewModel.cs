20c9edf4c2c1e4d8a5fd89dbe3e419c1b6eeb8f8
Landon Clark <landon.d.clark@gmail.com>
2019-01-24 12:00:21 -0700

    Create ErrorViewModel.cs

---
+++ PlanCheck.Script/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PlanCheck
+{
+    public class ErrorViewModel : ViewModelBase
+    {
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public int Severity { get; set; }
+    }
+}
