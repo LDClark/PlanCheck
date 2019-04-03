f34d1ad520fc89e9e6efd969780d033fe1962eb9
Landon Clark <landon.d.clark@gmail.com>
2019-04-03 16:51:56 -0600

    Update PQMVolumeAtDose.cs

--- PlanCheck.Script/Calculators/PQMVolumeAtDose.cs
+++ PlanCheck.Script/Calculators/PQMVolumeAtDose.cs
@@ -6,10 +6,10 @@ namespace PlanCheck.Calculators
 {
     class PQMVolumeAtDose
     {
-        public static string GetVolumeAtDose(StructureSet structureSet, PlanningItem planningItem, Structure evalStructure, MatchCollection testMatch, Group evalunit)
+        public static string GetVolumeAtDose(StructureSet structureSet, PlanningItemViewModel planningItem, Structure evalStructure, MatchCollection testMatch, Group evalunit)
         {
             //check for sufficient sampling and dose coverage
-            DVHData dvh = planningItem.GetDVHCumulativeData(evalStructure, DoseValuePresentation.Absolute, VolumePresentation.Relative, 0.1);
+            DVHData dvh = planningItem.PlanningItemObject.GetDVHCumulativeData(evalStructure, DoseValuePresentation.Absolute, VolumePresentation.Relative, 0.1);
             //MessageBox.Show(evalStructure.Id + "- Eval unit: " + evalunit.Value.ToString() + "Achieved unit: " + dvAchieved.UnitAsString + " - Sampling coverage: " + dvh.SamplingCoverage.ToString() + " Coverage: " + dvh.Coverage.ToString());
             if ((dvh.SamplingCoverage< 0.9) || (dvh.Coverage< 0.9))
             {
@@ -24,7 +24,18 @@ namespace PlanCheck.Calculators
             double volume = double.Parse(eval.Value);
             VolumePresentation vpFinal = (evalunit.Value.CompareTo("%") == 0) ? VolumePresentation.Relative : VolumePresentation.AbsoluteCm3;
             DoseValuePresentation dvpFinal = (evalunit.Value.CompareTo("%") == 0) ? DoseValuePresentation.Relative : DoseValuePresentation.Absolute;
-            double volumeAchieved = planningItem.GetVolumeAtDose(evalStructure, dv, vpFinal);
+            if (planningItem.PlanningItemObject is PlanSum)
+            {
+                double planDoseDouble = 0;
+                PlanSum planSum = (PlanSum)planningItem.PlanningItemObject;
+                foreach (PlanSetup planSetup in planSum.PlanSetups)
+                {
+                    planDoseDouble += planSetup.TotalDose.Dose;
+                }
+                dv = new DoseValue(planDoseDouble, DoseValue.DoseUnit.cGy);
+            }
+
+            double volumeAchieved = planningItem.PlanningItemObject.GetVolumeAtDose(evalStructure, dv, vpFinal);
             return string.Format("{0:0.00} {1}", volumeAchieved, evalunit.Value);   // todo: better formatting based on VolumePresentation
 //#if false
 //            string message = string.Format("{0} - Dose unit = {1}, Volume Presentation = {2}, vpFinal = {3}, dvpFinal ={4}", 
